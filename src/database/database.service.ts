4aac88defe90bf4235f2c55d4f0af76a68c1ea14
mahmutcant <mahcan012@gmail.com>
2024-09-30 07:35:36 +0300

    mock users added

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -43,15 +43,32 @@ export class DatabaseService implements OnModuleInit {
   }
 
   private async insertMockData() {
-    /*const mockData = [
+    const mockData = [
       { name: 'Mahmut Can', surname: 'Tuncer', email: 'mahcan012@gmail.com', password: await bcrypt.hash('123qwe', 10), phone: '5388208499', district: 'Adana', role: 'admin' },
-      { name: 'Aybars', surname: 'Savrun', email: 'aybars.savrun@gmail.com', password: await bcrypt.hash('123456', 10), phone: '6548575415', district: 'Osmaniye', role: 'user' },
-      { name: 'Sefa Enes', surname: 'Özoğuz', email: 'sefaenes.ozoguz@gmail.com', password: await bcrypt.hash('12345678', 10), phone: '123412344', district: 'Adana', role: 'user' },
+      { name: 'Fernando', surname: 'Muslera', email: 'f.muslera@gmail.com', password: await bcrypt.hash('123qwe', 10), phone: '5388208499', district: 'Mersin', role: 'user' },
+      { name: 'Cenk', surname: 'Şen', email: 'cenk.sen@gmail.com', password: await bcrypt.hash('123qwe', 10), phone: '5388208499', district: 'Adana', role: 'user' },
+      { name: 'Olivier', surname: 'Ntcham', email: 'mahcan0125@gmail.com', password: await bcrypt.hash('123qwe', 10), phone: '5388208499', district: 'Mersin', role: 'user' },
+      { name: 'Uğurcan', surname: 'Çakır', email: 'mahcan0126@gmail.com', password: await bcrypt.hash('123qwe', 10), phone: '5388208499', district: 'Adana', role: 'user' },
+      { name: 'Rafa', surname: 'Silva', email: 'mahcan0127@gmail.com', password: await bcrypt.hash('123qwe', 10), phone: '5388208499', district: 'Mersin', role: 'user' },
+      { name: 'Nuno Da', surname: 'Costa', email: 'nuno.da.costa@gmail.com', password: await bcrypt.hash('123qwe', 10), phone: '5388208499', district: 'Adana', role: 'user' },
+      { name: 'Edin', surname: 'Dzeko', email: 'edin.dzeko@gmail.com', password: await bcrypt.hash('123qwe', 10), phone: '5388208499', district: 'Adana', role: 'user' },
+      { name: 'Dries', surname: 'Mertens', email: 'dries.mertens@gmail.com', password: await bcrypt.hash('123qwe', 10), phone: '5388208499', district: 'Adana', role: 'user' },
+      { name: 'Marc', surname: 'Bola', email: 'marc.bola@gmail.com', password: await bcrypt.hash('123qwe', 10), phone: '5388208499', district: 'Adana', role: 'user' },
+      { name: 'Jonas', surname: 'Svensson', email: 'jonas.svensson@gmail.com', password: await bcrypt.hash('123qwe', 10), phone: '5388208499', district: 'Adana', role: 'user' },
+      { name: 'Victor', surname: 'Nelsson', email: 'victor.nelsson@gmail.com', password: await bcrypt.hash('123qwe', 10), phone: '5388208499', district: 'Adana', role: 'user' },
+      { name: 'Allan Saint', surname: 'Maximin', email: 'allan.maxi8@gmail.com', password: await bcrypt.hash('123qwe', 10), phone: '5388208499', district: 'Adana', role: 'user' },
+      { name: 'Barış Alper', surname: 'Yılmaz', email: 'bay@gmail.com', password: await bcrypt.hash('123qwe', 10), phone: '5388208499', district: 'Adana', role: 'user' },
+      { name: 'Ahmet Can', surname: 'Yıldırım', email: 'ahmetcan@gmail.com', password: await bcrypt.hash('123qwe', 10), phone: '5388208499', district: 'Adana', role: 'user' },
+      { name: 'Mahmut Can', surname: 'Yıldırım', email: 'mahcan0122@gmail.com', password: await bcrypt.hash('123qwe', 10), phone: '5388208499', district: 'Adana', role: 'user' },
+      { name: 'Mehmet Can', surname: 'Yıldırım', email: 'mehmetcan.yildirim@gmail.com', password: await bcrypt.hash('123qwe', 10), phone: '5388208499', district: 'Adana', role: 'user' },
+      { name: 'Yunus Can', surname: 'Yıldırım', email: 'yunus_can_yildirim@gmail.com', password: await bcrypt.hash('123qwe', 10), phone: '5388208499', district: 'Adana', role: 'user' },
+      { name: 'Murat', surname: 'Yıldırım', email: 'murat.yildirim@gmail.com', password: await bcrypt.hash('123qwe', 10), phone: '5388208499', district: 'Adana', role: 'user' },
+      { name: 'Gökhan', surname: 'Yıkılkan', email: 'gokhan.yikilkan@gmail.com', password: await bcrypt.hash('123qwe', 10), phone: '5388208499', district: 'Adana', role: 'user' },
     ];
   
     for (const data of mockData) {
       await this.db.none('INSERT INTO users(name, surname, email, password, phone, district, role) VALUES(${name}, ${surname}, ${email}, ${password}, ${phone}, ${district}, ${role})', data);
-    }*/
+    }
   }
 
   getDb() {
